10099420278be4aae7ba2805f2f70296dbb474d3
danbe <danbe8080@gmail.com>
2022-04-05 19:00:14 +0900

    paint 반응형 기능 완료

--- js/paint.js
+++ js/paint.js
@@ -2,6 +2,11 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("colors");
 const initialColor = document.querySelector("#initialColor");
+const paintModalWrap = document.querySelector("#paintModalWrap");
+const fill = document.querySelector("#fillOption");
+const brush = document.querySelector("#brushOption");
+const range = document.querySelector("#rangeBrush");
+const saveBtn = document.querySelector("#downloadOption");
 
 // 캔버스 크키와 색 초기화
 const INITIAL_COLOR = initialColor.style.backgroundColor;
@@ -10,6 +15,19 @@ let CANVAS_HEIGHT = canvas.offsetHeight;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
+
+// paintModal창 오픈, 닫기
+let paintModal = false;
+
+const onPaintModal = () => {
+  if (!paintModal) {
+    paintModalWrap.style.transform = "translateX(0px)";
+    paintModal = true;
+  } else {
+    paintModalWrap.style.transform = "translateX(-10000px)";
+    paintModal = false;
+  }
+};
 
 // 선 크기 초기화
 ctx.lineWidth = 3;
@@ -28,7 +46,21 @@ let filling = false;
 
 // 웹사이트 크키가 변할 시 일어날 함수
 const checkCanvasSize = () => {
-  location.reload();
+  CANVAS_WIDTH = canvas.offsetWidth;
+  CANVAS_HEIGHT = canvas.offsetHeight;
+
+  Array.from(colors).forEach((color) => {
+    color.classList.remove("selete");
+  });
+  initialColor.classList.add("selete");
+
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
+
+  ctx.lineWidth = range.value;
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillStyle = INITIAL_COLOR;
 };
 
 // 그리기 동작 함수들
@@ -97,6 +129,61 @@ const handleCanvasClick = () => {
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
 };
+
+// 브러쉬, 채우기 모드 변경
+const handleModeClick = (event) => {
+  const target = event.target.classList[1];
+  if (target === "bi-brush-fill") {
+    filling = false;
+    range.removeAttribute("disabled", "");
+  } else if (target === "bi-paint-bucket") {
+    filling = true;
+    range.setAttribute("disabled", "");
+  }
+};
+
+fill.addEventListener("click", handleModeClick);
+brush.addEventListener("click", handleModeClick);
+
+// 컬러 변경
+const handleColorClick = (event) => {
+  const chooseColor = event.target;
+  const color = chooseColor.style.backgroundColor;
+  Array.from(colors).forEach((col) => {
+    col.classList.remove("selete");
+  });
+  chooseColor.classList.add("selete");
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+};
+
+// color가 여러개이기 때문에 for으로 돌려줌
+Array.from(colors).forEach((color) => {
+  color.addEventListener("click", handleColorClick);
+});
+
+// 그림 파일 다운로드
+const handleSaveClick = () => {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "Paint";
+  link.click();
+};
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
+}
+
+const handleRangeChange = (event) => {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+};
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting); // 마우스 클릭 O
@@ -112,92 +199,4 @@ if (canvas) {
   window.addEventListener("resize", checkCanvasSize);
 }
 
-// const range = document.querySelector("#rangeBrush");
-// const mode = document.querySelector("#fillOption");
-// const saveBtn = document.querySelector("#downloadOption");
-// const resetBtn = document.querySelector("#reBtn");
-
-// let reset = false;
-
-// /* 마우스(PC)) */
-// function handleCM(event) {
-//   event.preventDefault();
-// }
-
-// function handleColorClick(event) {
-//   const chooseColor = event.target;
-//   const color = chooseColor.style.backgroundColor;
-//   Array.from(colors).forEach((col) => {
-//     col.classList.remove("selete");
-//   });
-//   chooseColor.classList.add("selete");
-//   ctx.strokeStyle = color;
-//   ctx.fillStyle = color;
-// }
-
-// function handleCanvasClick() {
-//   if (filling) {
-//     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-//   }
-// }
-// function handleRangeChange(event) {
-//   const size = event.target.value;
-//   ctx.lineWidth = size;
-// }
-// function handleModeClick() {
-//   if (filling === true) {
-//     filling = false;
-//     mode.innerText = "Fill";
-//   } else {
-//     filling = true;
-//     mode.innerText = "Paint";
-//   }
-// }
-// function handleSaveClick() {
-//   const image = canvas.toDataURL();
-//   const link = document.createElement("a");
-//   link.href = image;
-//   link.download = "Paint";
-//   link.click();
-// }
-
-// function handleResetClick() {
-//   ctx.fillStyle = `#ffffff`;
-//   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-//   ctx.fillStyle = INITIAL_COLOR;
-//   ctx.beginPath();
-// }
-
-// if (mode) {
-//   mode.addEventListener("click", handleModeClick);
-// }
-// if (saveBtn) {
-//   saveBtn.addEventListener("click", handleSaveClick);
-// }
-// if (range) {
-//   range.addEventListener("input", handleRangeChange);
-// }
-// if (resetBtn) {
-//   resetBtn.addEventListener("click", handleResetClick);
-// }
-// Array.from(colors).forEach((color) => {
-//   color.addEventListener("click", handleColorClick);
-// });
-
-// function checkCanvasSize() {
-//   CANVAS_WIDTH = canvas.offsetWidth;
-//   CANVAS_HEIGHT = canvas.offsetHeight;
-
-//   Array.from(colors).forEach((color) => {
-//     color.classList.remove("selete");
-//   });
-//   initialColor.classList.add("selete");
-
-//   canvas.width = CANVAS_WIDTH;
-//   canvas.height = CANVAS_HEIGHT;
-
-//   ctx.lineWidth = range.value;
-//   ctx.fillStyle = "#ffffff";
-//   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-//   ctx.fillStyle = INITIAL_COLOR;
-// }
+paintWrap.addEventListener("click", onPaintModal);
